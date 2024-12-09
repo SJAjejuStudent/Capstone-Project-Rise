d4da1fd45b9f020b36d93d8e970ca3dbac0f9766
SJA Student <s23290752@sjajeju.kr>
2024-12-09 14:50:06 +0900

    Fix bug of page

--- public/styleChange.js
+++ public/styleChange.js
@@ -3,24 +3,25 @@ let lightLink = "style.css";
 var links = document.getElementsByClassName('styles');
 
 function darkMode() {
-    if (window.name === "style.css" || window.name == null) {
+    if (window.name === "style.css" || window.name == ``) {
         currentLink = darkLink;
     } else if (window.name === "dark.css") {
         currentLink = lightLink;
     }
     window.name = currentLink;
     Array.from(links).forEach(link => {
-        link.href = currentLink;
+        link.href = window.name;
     });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    if (window.name = "") {
+    if (window.name == ``) {
         Array.from(links).forEach(link => {
             link.href = "style.css";
         });
-    }
+    } else {
     Array.from(links).forEach(link => {
         link.href = window.name;
     });
+}
 })
